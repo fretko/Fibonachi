7ff913782a217fb3c57355738f9f271f8b766872
fretko <100627200+fretko@users.noreply.github.com>
2022-06-14 23:56:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+n=int(input())
+k=int(input())
+spisok="m"
+krug=1
+spisok=spisok.replace('m','S')
+while krug!=n:
+    krug+=1
+    spisok=spisok.replace("m", '%temp%')
+    spisok=spisok.replace('S', 'S'+'m'*k)
+    spisok=spisok.replace('%temp%','S')
+print(spisok)
